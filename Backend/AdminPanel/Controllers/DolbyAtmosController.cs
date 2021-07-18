8dceb2e1fd44f5dfc344147e26831fbcb191efdd
nazimhasanov <nazimnh@academy.edu.az>
2021-07-18 18:55:17 +0400

    init admin panel

---
+++ Backend/AdminPanel/Controllers/DolbyAtmosController.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Controllers
+{
+    public class DolbyAtmosController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public DolbyAtmosController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var dolbyAtmos = _dbContext.DolbyAtmos.ToList();
+            return View(dolbyAtmos);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
